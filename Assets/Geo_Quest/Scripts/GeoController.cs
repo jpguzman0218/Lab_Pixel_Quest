6691802a20798cdb49abb56f839c3e3139901706
jpguzman0218 <jpguzman0218@gmail.com>
2026-02-14 16:30:28 -0500

    2/14/2025

---
+++ Assets/Geo_Quest/Scripts/GeoController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeoController : MonoBehaviour
+{
+
+    string hello = "Hello ";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        string world = "World";
+        Debug.Log(hello + world);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
